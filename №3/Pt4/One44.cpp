269a72a33f42737df286a3ca1a5de81ea9ef637a
Ekky <dm.lyapin.2012@gmail.com>
2022-09-18 22:57:36 +0500

    Четность цифр на позициях числа.
    One44. Задача взята с сайта http://ptaskbook.com/ru/
    
    С клавиатуры вводится шестизначное натуральное число. Программа определяет, действительно ли, что на четных позициях в числе стоят четные цифры, а на нечетных позициях - нечетные. Нумерация цифр в числе производится слева направо и начинается с 1. В качестве ответа выводится True ( 1 ) или False ( 0 ).
    
    Две папки. В Pt4 находятся два файла, которые нужно переместить в папку, в которой находится Programming Taskbook. В VStudio находится программа, работающая в Visual Studio.

---
+++ №3/Pt4/One44.cpp
@@ -0,0 +1,27 @@
+#include "pt4.h"
+using namespace std;
+
+void Solve()
+{
+    Task("One44");
+    int kol = 0, n, i = 1, r = 100000;
+    pt >> n;
+    while (i != 7)
+    {
+        if (((n / r % 2 == 0) && (i % 2 == 0)) || ((n / r % 2 != 0) && (i % 2 != 0)))
+        {
+            kol += 1;
+        }
+        i += 1;
+        r /= 10;
+    }
+    if (kol == 6)
+    {
+        pt << true;
+    }
+    else
+    {
+        pt << false;
+    }
+
+}
